598b0822747f8d8eb0fcbabcd1af21b7d5f6bdbd
impangt <pt.pangtao@gmail.com>
2017-04-20 16:15:52 +0800

    adjust the display details of two windows.

--- drawGraph.py
+++ drawGraph.py
@@ -2,19 +2,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.patches import Rectangle
 
+
 class DraggableRectangle(object):
-    def __init__(self):
-        self.ax = plt.gca()
-        self.rect = Rectangle((0.5,0), 0.22, 2,facecolor='g', alpha=0.2)
-        self.rectl = Rectangle((0.5-0.05, 0), 0.05, 0.2, facecolor='b', alpha=0.8)
-        self.rectr = Rectangle((0.5+0.22, 0), 0.05, 0.2, facecolor='b', alpha=0.8)
-        self.rect_x = 0.5
+    def __init__(self, axes, rectwidth=100, lrwidth=10, recty=100):
+        self.ax = axes  # plt.gca()
+
+        self.rect_x = axes.get_xlim()[1] - rectwidth - lrwidth
+        self.rect = Rectangle((self.rect_x, 0), rectwidth, recty, facecolor='g', alpha=0.2)
+        self.rectl = Rectangle((self.rect_x - lrwidth, 0), lrwidth, recty, facecolor='b', alpha=0.8)
+        self.rectr = Rectangle((self.rect_x + rectwidth, 0), lrwidth, recty, facecolor='b', alpha=0.8)
 
         self.ax.add_patch(self.rect)
         self.ax.add_patch(self.rectl)
         self.ax.add_patch(self.rectr)
 
-        self.press = None # 0,
+        self.rect_width = rectwidth
+        self.press = None  # 0,
         self.pressl = None
         self.pressr = None
 
@@ -22,9 +25,11 @@ class DraggableRectangle(object):
         self.ax.figure.canvas.mpl_connect('button_release_event', self.on_release)
         self.ax.figure.canvas.mpl_connect('motion_notify_event', self.on_motion)
 
+        # self.reset_right()
 
     def on_press(self, event):
         if event.inaxes != self.rect.axes: return
+
         contains, attrd = self.rect.contains(event)
         containsl, attrdl = self.rectl.contains(event)
         containsr, attrdr = self.rectr.contains(event)
@@ -52,42 +57,102 @@ class DraggableRectangle(object):
         if self.press is None and self.pressl is None and self.pressr is None: return
         if event.inaxes != self.rect.axes and event.inaxes != self.rectl.axes and event.inaxes != self.rectr.axes: return
 
+        rectwidth = self.rect_width
         if self.press != None:
             x0, y0, xpress, ypress = self.press
             dx = event.xdata - xpress
-            self.rect_x = x0+dx
+            rectl_x = x0 + dx  - self.rectl.get_width()
+
+            # to tell the left edge overriding
+            if rectl_x < 0:
+                rectl_x = 0
+            # to tell the right edge overriding
+            if rectl_x + self.rectl.get_width() + rectwidth + self.rectr.get_width() > self.ax.get_xlim()[1]:
+                rectl_x = self.rect_x - self.rectl.get_width()
+
+            self.rect_x = rectl_x + self.rectl.get_width()
             self.rect.set_x(self.rect_x)
-            self.rectl.set_x(self.rect_x-self.rectl.get_width())
-            self.rectr.set_x(self.rect_x+self.rect.get_width())
+            self.rectl.set_x(rectl_x)
+            self.rectr.set_x(self.rect_x + self.rect.get_width())
             # self.rect.figure.canvas.draw()
         elif self.pressl != None:
-            if event.xdata >= self.rectr.get_x()-0.1: return
+            if event.xdata >= self.rectr.get_x() - 10:
+                self.rect_width = 10
+                return
+
             xl, yl, xpress, ypress = self.pressl
             dx = event.xdata - xpress
-            self.rectl.set_x(xl + dx - self.rectl.get_width())
+            rectl_x = xl + dx #- self.rectl.get_width()
+
+            # to tell the left edge overriding
+            if rectl_x < 0:
+                self.rect_width = self.rectr.get_x() - self.rectl.get_width()
+                rectl_x = 0
+
+            self.rectl.set_x(rectl_x)
             # draw rect
-            self.rect.set_x(xl + dx)
+            self.rect.set_x(rectl_x + self.rectl.get_width())
             xr = self.rectr.get_x()
-            recwidth = xr-xl-dx
-            self.rect.set_width(recwidth)
+            rectwidth = xr - rectl_x - self.rectl.get_width()
             # self.rectl.figure.canvas.draw()
         elif self.pressr != None:
-            if event.xdata <= self.rectl.get_x()+self.rectl.get_width()+0.1: return
+            if event.xdata <= self.rect_x + 10:
+                self.rect_width = 10
+                return
+
             xr, yr, xpress, ypress = self.pressr
             dx = event.xdata - xpress
-            self.rectr.set_x(xr+dx)
+            rectr_x = xr + dx
+
+            # to tell the right edge overriding
+            xlim = self.ax.get_xlim()[1]
+            if rectr_x + self.rectr.get_width() > xlim:
+                self.rect_width = xlim -self.rectl.get_x() - self.rectl.get_width()
+                rectr_x = xlim - self.rectr.get_width()
+
+            self.rectr.set_x(rectr_x)
             # draw rect
-            xl = self.rectl.get_x()
-            recwidth = xr+dx - xl - self.rectl.get_width()
-            self.rect.set_width(recwidth)
+            rectwidth = rectr_x - self.rectl.get_x() - self.rectl.get_width()
             # self.rectr.figure.canvas.draw()
 
-        self.rect.figure.canvas.draw()# why we just draw rect here, but rectl and rectr are drawed too ?
+        self.rect.set_width(rectwidth)
+        self.rect_width = rectwidth
+        self.ax.figure.canvas.draw()
+        # self.rect.figure.canvas.draw()  # why we just draw rect here, but rectl and rectr are drawed too ?
 
-# t = np.arange(0, 8, .01)
-# s = np.sin(3 * np.pi * t)
-# plt.plot(t,s)
-#
-# plt.axis([0, 2, 0, 2])
-# a = DraggableRectangle()
-# plt.show()
+    def reset_left(self):
+        self.rectl.set_x(0.0)
+        self.rect_x = self.rectl.get_width()
+        self.rect.set_x(self.rect_x)
+        self.rectr.set_x(self.rect_x + self.rect_width)
+
+    def reset_right(self):
+        xlim = self.ax.get_xlim()[1]
+        self.rectr.set_x(xlim - self.rectl.get_width())
+        self.rect_x = xlim - self.rectl.get_width()-self.rect_width
+        self.rect.set_x(self.rect_x)
+        self.rectl.set_x(self.rect_x - self.rectl.get_width())
+
+    def reset_full(self):
+        self.rectl.set_x(0.0)
+        xlim = self.ax.get_xlim()[1]
+        self.rect_x = self.rectl.get_width()
+        self.rect.set_x(self.rect_x)
+        self.rectr.set_x(xlim - self.rectl.get_width())
+        self.rect_width = xlim - self.rectl.get_width() - self.rect_x
+        self.rect.set_width(self.rect_width)
+        self.ax.figure.canvas.draw()
+
+    def get_rect_x1(self):
+        return int(self.rectl.get_x())
+
+    def get_rect_x2(self):
+        return int(self.rectr.get_x()+self.rectr.get_width())
+
+        # t = np.arange(0, 8, .01)
+        # s = np.sin(3 * np.pi * t)
+        # plt.plot(t,s)
+        #
+        # plt.axis([0, 2, 0, 2])
+        # a = DraggableRectangle()
+        # plt.show()
--- mainWindow.py
+++ mainWindow.py
@@ -11,97 +11,14 @@ from selectPoliciesDlg import PoliciesDialog
 import deduceEarning as dE
 import drawGraph
 
-from matplotlib.patches import Rectangle
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from PyQt5.QtCore import QDate
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
 
-class DraggableRectangle(object):
-    def __init__(self, axes):
-        self.ax = axes # plt.gca()
-        self.rect = Rectangle((0.5,0), 0.22, 2,facecolor='g', alpha=0.2)
-        self.rectl = Rectangle((0.5-0.05, 0), 0.05, 0.2, facecolor='b', alpha=0.8)
-        self.rectr = Rectangle((0.5+0.22, 0), 0.05, 0.2, facecolor='b', alpha=0.8)
-        self.rect_x = 0.5
-
-        self.ax.add_patch(self.rect)
-        self.ax.add_patch(self.rectl)
-        self.ax.add_patch(self.rectr)
-
-        self.press = None # 0,
-        self.pressl = None
-        self.pressr = None
-
-        self.ax.figure.canvas.mpl_connect('button_press_event', self.on_press)
-        self.ax.figure.canvas.mpl_connect('button_release_event', self.on_release)
-        self.ax.figure.canvas.mpl_connect('motion_notify_event', self.on_motion)
-
-
-    def on_press(self, event):
-        if event.inaxes != self.rect.axes: return
-        contains, attrd = self.rect.contains(event)
-        containsl, attrdl = self.rectl.contains(event)
-        containsr, attrdr = self.rectr.contains(event)
-        if contains:
-            x0, y0 = self.rect.xy
-            self.press = x0, y0, event.xdata, event.ydata
-        elif containsl:
-            xl, yl = self.rectl.xy
-            self.pressl = xl, yl, event.xdata, event.ydata
-        elif containsr:
-            xr, yr = self.rectr.xy
-            self.pressr = xr, yr, event.xdata, event.ydata
-        else:
-            return
-
-    def on_release(self, event):
-        # print ('release')
-        self.press = None
-        self.pressl = None
-        self.pressr = None
-        self.ax.figure.canvas.draw()
-
-    def on_motion(self, event):
-        'on motion we will move the rect if the mouse is over us'
-        if self.press is None and self.pressl is None and self.pressr is None: return
-        if event.inaxes != self.rect.axes and event.inaxes != self.rectl.axes and event.inaxes != self.rectr.axes: return
-
-        if self.press != None:
-            x0, y0, xpress, ypress = self.press
-            dx = event.xdata - xpress
-            self.rect_x = x0+dx
-            self.rect.set_x(self.rect_x)
-            self.rectl.set_x(self.rect_x-self.rectl.get_width())
-            self.rectr.set_x(self.rect_x+self.rect.get_width())
-            # self.rect.figure.canvas.draw()
-        elif self.pressl != None:
-            if event.xdata >= self.rectr.get_x()-0.1: return
-            xl, yl, xpress, ypress = self.pressl
-            dx = event.xdata - xpress
-            self.rectl.set_x(xl + dx - self.rectl.get_width())
-            # draw rect
-            self.rect.set_x(xl + dx)
-            xr = self.rectr.get_x()
-            recwidth = xr-xl-dx
-            self.rect.set_width(recwidth)
-            # self.rectl.figure.canvas.draw()
-        elif self.pressr != None:
-            if event.xdata <= self.rectl.get_x()+self.rectl.get_width()+0.1: return
-            xr, yr, xpress, ypress = self.pressr
-            dx = event.xdata - xpress
-            self.rectr.set_x(xr+dx)
-            # draw rect
-            xl = self.rectl.get_x()
-            recwidth = xr+dx - xl - self.rectl.get_width()
-            self.rect.set_width(recwidth)
-            # self.rectr.figure.canvas.draw()
-
-        self.rect.figure.canvas.draw()# why we just draw rect here, but rectl and rectr are drawed too ?
-
 class MyCanvas(FigureCanvas):
-    """这是一个窗口部件，即QWidget（当然也是FigureCanvasAgg）"""
+    """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
     def __init__(self, parent=None):
         self.fig = plt.figure(facecolor='lightblue', dpi=100) # 这里设置figsize没有用！
         # 设置两个绘图区域，图像高度比例为4:1
@@ -125,19 +42,15 @@ class MyCanvas(FigureCanvas):
         self.zoomidx = 1
         self.showindicator = True
 
-        # plt.axis([0,5, 0, 1])
-        # dr = DraggableRectangle(self.axes2)
-        # plt.show()
-
+        self.dragrect = None
     # draw sin figure
     def compute_initial_figure(self):
-        t = arange(0.0, 3.0, 0.01)
-        s = sin(2 * pi * t)
-        self.axes.plot(t, s)
+        pass
 
     # the scope of zoom rate is [1, 10], 1 = no zoom , 2 = zoom to 90% , 3 = to 80%, ... 10 = 10%
     def drawfigure1(self, dataframe):
-        # plt.clf()
+        self.axes.clear() # It's so hard i find this way to clear axes...
+
         length1 = len(dataframe.index)
 
         viewx = np.arange(0, length1, 1)
@@ -152,15 +65,11 @@ class MyCanvas(FigureCanvas):
         for i in np.arange(0, length1, step):
             l1.append(dataframe.index[i])
 
-        self.axes.set_xlim(0, length1)
+        self.axes.set_xlim(0, length1-1)
         self.axes.set_xticks(np.arange(0, length1, step))
         self.axes.set_xticklabels(l1)
         self.axes.plot(viewx, y, color='c')
-        # plt.xlim(0, length1)
-        # plt.xticks(np.arange(0, length1, step), l1)  # , rotation=30)
-        # plt.ylabel('day price')
-        # plt.plot(viewx, y, color='c')  # , linestyle='dashed', marker='o')
-        #
+
         self.axes.grid()
         self.draw()  # 在窗体内绘图，如果直接使用 plot.show()则会另外开启绘图窗口
         return True
@@ -195,21 +104,8 @@ class MyCanvas(FigureCanvas):
         viewx = np.arange(0, length1, 1)
         y = dataframe.iloc[:, 0]
 
-        # list the display label of x-axis
-        l1 = []
-        step = int(length1 / 5)  # 如果选择的日期数量少于step，则会出错
-        if step == 0:
-            return False
-
-        for i in np.arange(0, length1, step):
-            l1.append(dataframe.index[i])
-
-        self.axes2.set_xlim(0, length1)
+        self.axes2.set_xlim(0, length1-1)
         self.axes2.plot(viewx, y, color='lightgrey')
-
-        plt.axis([0, 5, 0, 1])
-        dr = DraggableRectangle(self.axes2)
-
         self.draw()
         return True
 
@@ -227,12 +123,35 @@ class ApplicationWindow(QMainWindow):
         self.showindicator = True
 
         self.mycanvas = MyCanvas(self)  # mainAppWindow.ui.mainWidget)#, width=5, height=4, dpi=100)
+        self.mycanvas.mpl_connect('button_press_event', self.on_mousebutton_press)
+        self.mycanvas.mpl_connect('button_release_event', self.on_mousebutton_release)
+
         self.ui.verticalLayout.addWidget(self.mycanvas)
         self.ui.loadstButton.setDisabled(True)
 
         self.dataframe = pd.DataFrame()
         self.originaldatalength = 0
         self.pdfile = 'config\\policyselections'
+
+    def on_mousebutton_press(self,event):
+        if event.inaxes != self.mycanvas.axes2: return
+
+        # boxpoint = event.artist.get_bbox().get_points()
+        self.mycanvas.dragrect.on_press(event)
+
+    def on_mousebutton_release(self,event):
+        if event.inaxes != self.mycanvas.axes2: return
+
+        # set data frame according to the drag rectangle datas
+        x1 = self.mycanvas.dragrect.get_rect_x1()
+        x2 = self.mycanvas.dragrect.get_rect_x2()
+
+        print(x1,x2,x2-x1)
+
+        view_dateframe = self.dataframe.iloc[x1:x2, ]
+        self.mycanvas.drawfigure1(view_dateframe)
+        # set the dateEdit controller
+        self.setEditDate(self.dataframe.index[x1], self.dataframe.index[x2])
 
     def openButtonClicked(self):
         self.targetDir, filetype = QFileDialog.getOpenFileName(self,
@@ -245,17 +164,24 @@ class ApplicationWindow(QMainWindow):
             self.dataframe['buy'] = 0.0
             self.dataframe['sell'] = 0.0
 
-            # set the dateEdit controller
-            self.setEditDate(self.dataframe.index[0], self.dataframe.index[-1])
-
-            self.originaldatalength = len(self.dataframe.index)
-            self.slidewindowsize = self.originaldatalength
-            self.ui.hslider.setRange(0, 0)
-            print("origin length is ", self.slidewindowsize)
-
-            # draw the figure
-            self.mycanvas.drawfigure1(self.dataframe)
+            # draw figure2 first, then we get the right axes2 x limit
             self.mycanvas.drawfigure2(self.dataframe)
+
+            # create drag rectangle in axes2
+            self.mycanvas.dragrect = drawGraph.DraggableRectangle(self.mycanvas.axes2)
+
+            # set data frame according to the drag rectangle datas
+            x1 = self.mycanvas.dragrect.get_rect_x1()
+            x2 = self.mycanvas.dragrect.get_rect_x2()
+            print(x1,x2)
+            view_dateframe = self.dataframe.iloc[x1:x2, ]
+
+            # set the dateEdit controller
+            print(self.dataframe.index[x1], self.dataframe.index[x2])
+            self.setEditDate(self.dataframe.index[x1], self.dataframe.index[x2])
+
+            # draw the figure1
+            self.mycanvas.drawfigure1(view_dateframe)
 
             # set buttons enable
             self.ui.loadstButton.setDisabled(False)
@@ -264,13 +190,14 @@ class ApplicationWindow(QMainWindow):
             self.ui.zoomoutButton.setDisabled(False)
 
     def iniButtonClicked(self):
-        self.mycanvas.drawfigure(self.dataframe)
+        self.mycanvas.drawfigure1(self.dataframe)
+        self.mycanvas.dragrect.reset_full()
 
     def zoomInButtonClicked(self):
         if self.slidewindowsize > 10:
             view_lenth = int(self.slidewindowsize * 8 / 10)
             view_dateframe = self.dataframe.iloc[self.view_start:view_lenth - 1, ]
-            self.mycanvas.drawfigure(view_dateframe)
+            self.mycanvas.drawfigure1(view_dateframe)
             self.slidewindowsize = view_lenth
             self.ui.hslider.setRange(0, self.originaldatalength - view_lenth)
             # set the dateEdit controller
@@ -281,7 +208,7 @@ class ApplicationWindow(QMainWindow):
         if self.slidewindowsize <= len(self.dataframe.index):
             view_lenth = int(self.slidewindowsize * 12 / 10)
             view_dateframe = self.dataframe.iloc[self.view_start:view_lenth, ]
-            self.mycanvas.drawfigure(view_dateframe)
+            self.mycanvas.drawfigure1(view_dateframe)
             self.slidewindowsize = view_lenth
             self.ui.hslider.setRange(0, self.originaldatalength - view_lenth)
             # set the dateEdit controller
@@ -340,7 +267,7 @@ class ApplicationWindow(QMainWindow):
         print('slider pos = ', pos)
         if pos + self.slidewindowsize <= self.originaldatalength + 1:
             view_dateframe = self.dataframe.iloc[pos:pos + self.slidewindowsize, ]
-            self.mycanvas.drawfigure(view_dateframe)
+            self.mycanvas.drawfigure1(view_dateframe)
             self.view_start = pos
             # set the dateEdit controller
             self.setEditDate(view_dateframe.index[0], view_dateframe.index[-1])
